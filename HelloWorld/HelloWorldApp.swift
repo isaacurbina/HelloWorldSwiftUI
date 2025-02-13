49ad36cf7518398aecda87e4c2e8d2d3032de5b3
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-02-12 20:03:03 -0600

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Isaac Urbina on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
