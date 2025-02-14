649aa68f6a4bd80545291c9e0be6b5c7036904ff
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-02-13 18:47:34 -0600

    22. SwiftUI Link

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -10,10 +10,34 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "face.smiling")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Congratulations!")
+            Link("Go to Apple",
+				 destination: URL(string:"https://www.apple.com")!)
+			Link("Call To Action",
+				 destination: URL(string: "tel:1234567890")!)
+			Link("Send an Email",
+				 destination: URL(string: "mailto:swiftui@apple.com")!)
+			.buttonStyle(.borderedProminent)
+			.buttonBorderShape(.capsule)
+			.controlSize(.large)
+			.tint(.pink)
+			Link("Credo Academy",
+				 destination: URL(string: "https://credo.academy")!)
+			.buttonStyle(.plain)
+			.padding()
+			.border(.primary, width: 2)
+			Link(destination: URL(string: "https://www.apple.com")!) {
+				HStack(spacing: 16) {
+					Image(systemName: "apple.logo")
+					Text("Apple Store")
+				}
+			}
+			.font(.largeTitle)
+			.foregroundStyle(.white)
+			.padding()
+			.padding(.horizontal)
+			.background(
+				Capsule().fill(.blue)
+			)
         }
         .padding()
     }
