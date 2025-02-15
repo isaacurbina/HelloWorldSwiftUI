e8ba5cde5937676a08588c64bf3daa306b455ae5
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-02-14 18:58:26 -0600

    24. How to load a remote image from the Internet? (#4)

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -8,12 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+	
+	// MARK: - variables
+	private let imageUrl : String = "https://credo.academy/credo-academy@3x.png"
+	
+	
+	// MARK: - body
+	
     var body: some View {
         VStack {
+			// text
 			Text("iOS")
 				.font(.system(size: 180))
 				.fontWeight(.black)
 				.foregroundStyle(.teal.gradient)
+			
+			// link
             Link("Go to Apple",
 				 destination: URL(string:"https://www.apple.com")!)
 			Link("Call To Action",
@@ -42,10 +52,79 @@ struct ContentView: View {
 			.background(
 				Capsule().fill(.blue)
 			)
+			
+			// image
+//			AsyncImage(url: URL(string: imageUrl), scale: 4.0) { image in
+//				image.imageModifier()
+//					
+//			} placeholder: {
+//				Image(systemName: "photo.circle.fill").iconModifier() 
+//					
+//			}
+//			.padding(40)
+			
+//			if #available(iOS 15.0, *) {
+//				AsyncImage(url: URL(string: imageUrl), scale: 2.0) { phase in
+//					// SUCCESS: the image successfully loaded.
+//					// FAILURE: the image failed to load with an error
+//					// EMPTY: no image is loaded
+//					
+//					if let image = phase.image {
+//						image.imageModifier()
+//					} else if phase.error != nil {
+//						Image(systemName: "ant.circle.fill").iconModifier()
+//					} else {
+//						Image(systemName: "photo.circle.fill").iconModifier()
+//					}
+//					
+//				} placeholder: {
+//					Image(systemName: "photo.circle.fill").iconModifier()
+//				}
+//				.padding(40)
+//			} else {
+//				// Fallback on earlier versions
+//			}
+			AsyncImage(url: URL(string: imageUrl), transaction: Transaction(animation: .spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0.25))) { phase in
+				switch phase {
+				case .success(let image) :
+					image.imageModifier()
+						//.transition(.move(edge: .bottom))
+						.transition(.slide)
+				case .failure(_):
+					Image(systemName: "ant.circle.fill").iconModifier()
+				case .empty:
+					Image(systemName: "photo.circle.fill").iconModifier()
+				@unknown default:
+					ProgressView()
+				}}
         }
         .padding()
-    }
+	}
 }
+
+
+// MARK: - extensions
+
+extension Image {
+	
+	func imageModifier() -> some View {
+		self
+			.resizable()
+			.scaledToFill()
+	}
+	
+	func iconModifier() -> some View {
+		self
+			.resizable()
+			.scaledToFill()
+			.frame( maxWidth: 120)
+			.foregroundColor(.purple)
+			.opacity(0.5)
+	}
+}
+
+
+// MARK: - preview
 
 #Preview {
     ContentView()
